1f53f245b51770ce15b5a1561ad1573e3c4a492d
Niels Henrik Hagen <nhhagen@gmail.com>
2018-02-11 00:22:33 +0100

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const temporal = require('temporal');
-const Drone = require('rolling-spider');
+const Drone = require('parrot-minidrone');
 const discovery = require('./discovery');
 const docopt = require('docopt');
 const promisify = require('./rolling-spider-promise');
@@ -10,83 +10,82 @@ var stdin = process.openStdin();
 
 const drones = new Map();
 
+function sleep(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, time);
+  });
+}
+
+
 async function connect(droneName) {
+  console.log('Welcome to Drony');
   console.log(`Connecting to ${droneName}`)
   if (!drones.has(droneName)) {
-    const droneDetails = await discovery.use(droneName);
-    const temporal = require('temporal');
-    const drone = promisify(new Drone(droneDetails));
-    await drone.connect();
-    await drone.setup();
-    await drone.flatTrim();
-  await drone.startPing();
-    drones.set(droneName, drone)
-    console.log(`Connected to ${droneName}`);
+    // const droneDetails = await discovery.use(droneName);
+    const drone = new Drone({ autoconnect: false, droneFilter: droneName });
+    drone.connect();
+    drone.on('connected', data => {
+      drones.set(droneName, drone);
+      console.log(`Connected to ${droneName}`, data);
+    });
   }
 }
 
 async function takeOff(droneName, drone) {
   console.log('Taking off');
-  await drone.takeOff();
-  await drone.flatTrim();
+  drone.takeOff();
+  await sleep(1000)
 }
 
 async function land(droneName, drone) {
   console.log('Land')
   await drone.land();
-  await drone.disconnect()
-  await drones.delete(droneName);
+}
+
+function setSpeed(speed, controls) {
+  return {
+    roll: controls.roll * speed,
+    pitch: controls.pitch * speed,
+    yaw: controls.yaw * speed,
+    altitude: controls.altitude * speed,
+  }
 }
 
 async function move(droneName, drone, args) {
-  console.log(args)
   const doc = `
     usage:
       move [options] <directions>...
 
     options:
-      --delay <delay>  delay [default: 500]
+      --time <time>    time [default: 500]
       --speed <speed>  speed [default: 3]
-      --steps <steps>  steps [default: 5]
   `;
+
   const options = docopt.docopt(doc, { argv: args, options_first: true, exit: false });
 
   const directions = options['<directions>'];
-  const delay = parseFloat(options['--delay']);
-  const valid = new Set([
-    'turnRight',
-    'turnLeft',
-    'forward',
-    'backward',
-    'left',
-    'right',
-    'up',
-    'down'
+  const time = parseFloat(options['--time']);
+  const speed = parseFloat(options['--speed']);
+  const valid = new Map([
+    ['forward', { roll: 0, pitch: 1, yaw: 0 }],
+    ['backward', { roll: 0, pitch: -1, yaw: 0 }],
+    ['left', { roll: -1, pitch: 0, yaw: 0 }],
+    ['right', { roll: 1, pitch: 0, yaw: 0 }],
+    ['hover', { roll: 0, pitch: 0, yaw: 0, altitude: 0 }],
+    ['up', { roll: 0, pitch: 0, yaw: 0, altitude: 1 }],
+    ['down', { roll: 0, pitch: 0, yaw: 0, altitude: -1 }],
   ]);
 
-  directions.forEach(async direction => {
+  for (let direction of directions) {
     console.log(`Moving ${direction}`);
-    await drone[direction]({ speed: parseFloat(options['--speed']), steps: parseFloat(options['--steps']) });
-    await drone.flatTrim();
-  });
-}
+    const controls = valid.get(direction);
+    if (controls) {
 
-async function turn(droneName, drone, args) {
-  const doc = `
-    usage:
-      turn [--speed <speed>] [--steps <steps>] <direction>
-
-    options:
-      --speed <speed>  speed [default: 3]
-      --steps <steps>  steps [default: 5]
-  `;
-  const options = docopt.docopt(doc, { argv: args, options_first: true, exit: false });
-
-  console.log(options);
-
-  const direction = options['<direction>'];
-  const directions = new Map([['left', opt => drone.turnLeft(opt)], ['right', opt => drone.turnRight(opt)]]);
-  await directions.get(direction)();
+      drone.setFlightParams(setSpeed(speed, controls));
+      await sleep(time);
+      drone.setFlightParams(valid.get('hover'))
+    }
+  }
 }
 
 function checkDrone(func) {
@@ -102,7 +101,7 @@ function checkDrone(func) {
 async function showOff(droneName) {
   await commands.connect(droneName);
   commands.takeOff(droneName);
-  await commands.move(droneName, [ '--speed', '30', '--steps', '20', 'forward', 'forward' ]);
+  await commands.move(droneName, ['--time', '1000', '--speed', '15', 'forward', 'left', 'backward', 'right' ]);
   await commands.land(droneName);
 }
 
@@ -111,20 +110,20 @@ const commands = {
   takeOff: checkDrone(takeOff),
   land: checkDrone(land),
   move: checkDrone(move),
-  turn: checkDrone(turn),
   showOff
 }
 
 const doc = `
 Usage:
-  drone <command> <name> [<args>...]
+  drone <command> [<args>...]
 `;
 
 stdin.addListener('data', async data => {
   try {
     const args = docopt.docopt(doc, { argv: data.toString().trim(), options_first: true, exit: false });
     const command = args['<command>'];
-    const droneName = args['<name>'];
+    // const droneName = args['<name>'];
+    const droneName = 'Mambo_612477'
     const commandArgs = args['<args>'];
     await commands[command](droneName, commandArgs);
   } catch (e) {
